ec0ed72d2ebb6e8a53d36f64489073715165c747
anasraza07 <ar470684@gmail.com>
2024-07-25 04:32:58 +0500

    project 02 bmi generator done as well as projects dashboard completed

---
+++ project02-bmiGenerator/app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('.js-bmi-form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const height = Number(document.querySelector('#js-height').value);
+    const weight = Number(document.querySelector('#js-weight').value);
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    form.style.marginBottom = '15px';
+    document.querySelector('.js-bmi').innerHTML = `Your BMI is <span>${bmi}</span>.`;
+    let bmiStatus;
+    if (bmi < 18.5) {
+        bmiStatus = 'Underweight';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        bmiStatus = 'Healthyweight';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        bmiStatus = 'Overweight';
+    } else {
+        bmiStatus = 'Obesity';
+    }
+    document.querySelector('.js-status').innerHTML = `<span>Status</span>: ${bmiStatus}`;
+})
